f5d9b860a1cce61a0941eb80c59640a21115ede8
= <gabriel.silveira@4all.com>
2018-05-25 15:47:25 -0300

    Created project structure

---
+++ MarvelAppExample/Utils.swift
@@ -0,0 +1,22 @@
+//
+//  Utils.swift
+//  MarvelAppExample
+//
+//  Created by Gabriel Miranda Silveira on 25/05/18.
+//  Copyright © 2018 GabrielSilveira. All rights reserved.
+//
+
+import Foundation
+
+struct Utils {
+    
+    func getValue(fromPlistFile plistFile:String, withKey key:String) -> String? {
+        guard let path = Bundle.main.path(forResource: plistFile, ofType: "plist") else{
+            return nil
+        }
+        if let plistDictionary = NSDictionary(contentsOfFile: path) {
+            return plistDictionary[key] as? String
+        }
+        return nil
+    }
+}
---
+++ MarvelAppExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelAppExample
+//
+//  Created by Gabriel Miranda Silveira on 25/05/18.
+//  Copyright © 2018 GabrielSilveira. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
